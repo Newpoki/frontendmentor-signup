94b1c1d7bb777b001a11ceab8c6cdbf4a05c6a4f
Jason Savelli <jason.savelli@mention.com>
2023-10-14 16:45:32 +0200

    task: add colors to theme

---
+++ src/ui-kit/theme/theme.ts
@@ -0,0 +1,13 @@
+export const theme = {
+    colors: {
+        primary: {
+            tomato: 'hsl(4, 100%, 67%)',
+        },
+        neutral: {
+            darkSlateGrey: 'hsl(234, 29%, 20%)',
+            charcoalGrey: 'hsl(235, 18%, 26%)',
+            grey: 'hsl(231, 7%, 60%)',
+            white: 'hsl(0, 0%, 100%)',
+        },
+    },
+} as const
