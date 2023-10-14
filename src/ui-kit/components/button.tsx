e576ca4942602cb43bcae270a003618f63d50e5f
Jason Savelli <jason.savelli@mention.com>
2023-10-14 21:32:40 +0200

    task: add focus state on button

--- src/ui-kit/components/button.tsx
+++ src/ui-kit/components/button.tsx
@@ -7,13 +7,18 @@ export const Button = (props: Props) => {
 }
 
 const Root = styled.button`
-    border: none;
+    border: 1px solid transparent;
     background-color: ${({ theme }) => `hsl(${theme.colors.neutral.darkSlateGrey})`};
     color: ${({ theme }) => `hsl(${theme.colors.neutral.white})`};
     padding: 18px 20px;
     font-weight: ${({ theme }) => theme.fonts.roboto.weights[700]};
     font-size: 16px;
     border-radius: 8px;
+    outline: none;
+
+    &:focus {
+        background: linear-gradient(to right, hsl(349, 100%, 84%), hsl(19, 100%, 76%));
+    }
 
     &:hover {
         cursor: pointer;
